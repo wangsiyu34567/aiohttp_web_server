c94eafffb7a96e360dfcd334d37988bc2adfdf39
Mr.wang <1035243646@qq.com>
2019-11-03 14:48:00 +0800

    修复了新建进程内配置修改不生效问题

--- apps/__init__.py
+++ apps/__init__.py
@@ -2,6 +2,9 @@ from .app01 import server as app01_server
 
 
 def run_server(name, sock):
+    from configs import bases
+    bases.SERVICE_TYPE = name
+
     if name == 'app01':
         app01_server.run(sock)
     else:
