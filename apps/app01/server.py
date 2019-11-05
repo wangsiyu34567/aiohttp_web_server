b8d67ab9900a616c65e6b42c9708b101886c8352
Mr.wang <1035243646@qq.com>
2019-11-05 19:55:33 +0800

    logging添加了basicConfig, Application添加了Loop

--- apps/app01/server.py
+++ apps/app01/server.py
@@ -1,4 +1,3 @@
-import sys
 import logging
 
 from aiohttp import web
@@ -7,16 +6,18 @@ from apps.app01.urls import sub_app
 from apps.app01.middlewares import get_middlewares
 from core.server.server_run import go
 from core.logging.base_log import get_logger
+from configs .bases import LOG_CONFIG
 
 
-logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+logging.basicConfig(level=LOG_CONFIG['console']['level'],
+                    format=LOG_CONFIG['console']['formatter'])
 
 
 async def web_server(loop, sock):
     middlewares = get_middlewares()
-    app = web.Application(middlewares=middlewares, logger=get_logger(__name__))
+    app = web.Application(loop=loop, middlewares=middlewares, logger=get_logger(__name__))
     app.add_subapp('/api', sub_app)
-    handler = await loop.create_server(app.make_handler(), sock=sock)
+    handler = await loop.create_server(app.make_handler(), sock=sock, backlog=100)  # 测试backlog可适当调低
     return handler
 
 
